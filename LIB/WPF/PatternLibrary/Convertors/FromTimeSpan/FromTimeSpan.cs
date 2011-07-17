d0cc90a96ada5ba1f3be0723790908eb752d1094
minatron <minatron.git@gmail.com>
2011-07-18 00:32:24 +0400

    delete lib

--- LIB/WPF/PatternLibrary/Convertors/FromTimeSpan/FromTimeSpan.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows.Data;
-namespace WPF.Patterns.Convertors
-{
-    public static class FromTimeSpan
-    {
-        public new static readonly IValueConverter ToString = new TimeSpanToStringConvertor();
-        public static readonly IValueConverter ToString_Smart = new TimeSpanToStringConvertor(2);
-    }
-}
