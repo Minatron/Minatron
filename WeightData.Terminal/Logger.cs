7653fddc38c0e842aed1362977b688ae0fb118dd
Developer <minatron.git@gmail.com>
2011-07-26 02:57:30 +0400

    nmodbus included

---
+++ WeightData.Terminal/Logger.cs
@@ -0,0 +1,79 @@
+﻿using Band.WindowsLogger;
+
+namespace Band.WeightData.Terminal
+{
+    public class Logger : ILogger
+    {
+        public enum EventID : ushort
+        {
+            None = 0,
+            LogBegin = 1,
+            LogClose = 2,
+            ServiceStart = 3,
+            ServiceStop = 4,
+            ServiceCrash = 13001,
+            ServiceCannotStart = 13002,
+        }
+
+        ILogger _log;
+
+        public Logger(string name)
+        {
+            _log = MultiLogger.Get(
+                new ILogger[]
+                 {
+                 ConsoleLogger.Get(),                                                                                          
+                 EventLogger.Get("WeightDataTerminalLog","WeightDataTerminal_"+name,true)
+                 }
+            );
+        }
+
+        public void Close()
+        {
+            _log.Close();
+        }
+
+        public void WriteMessage(EventID id)
+        {
+            WriteMessage((ushort)id, "");
+        }
+        public void WriteMessage(EventID id, object message)
+        {
+            WriteMessage((ushort)id, message);
+        }
+
+        public void WriteErrorMessage(ushort id, object message)
+        {
+            WriteMessage(id, message);
+        }
+        public void WriteWarningMessage(ushort id, object message)
+        {
+            WriteMessage(id, message);
+        }
+        public void WriteMessage(ushort id, object message)
+        {
+            if (message == null) message = "";
+
+            switch ((EventID)id)
+            {
+                case EventID.ServiceStart:
+                    _log.WriteMessage(id, @"Начало работы");
+                    break;
+                case EventID.ServiceStop:
+                    _log.WriteMessage(id, @"Завершение работы");
+                    break;
+                case EventID.ServiceCannotStart:
+                    _log.WriteErrorMessage(id, message);
+                    break;
+                case EventID.ServiceCrash:
+                    _log.WriteErrorMessage(id, message);
+                    break;
+                default:
+                    _log.WriteMessage(id, message);
+                    break;
+            }
+
+            _log.WriteMessage(id, message);
+        }
+    }
+}
---
+++ WeightData.Terminal/ServerConfig.cs
@@ -0,0 +1,35 @@
+﻿namespace Band.WeightData.Terminal
+{
+    public class ServerConfig
+    {
+        public int    ListenPort = 502; //502
+        public string Name = null;
+        public string DBConnectionString = null;
+
+        public ServerConfig(string[] args)
+        {
+            if ((args != null) && (args.Length > 0))
+            {
+                string[] param;
+                for (int i = 0; i < args.Length; i++)
+                {
+                    param = args[i].Split('=');
+                    switch (param[0])
+                    {
+                        case "port":
+                            int.TryParse(param[1],out ListenPort);
+                            break;
+                        case "db":
+                            DBConnectionString = param[1];
+                            break;
+                        case "name":
+                            Name = param[1];
+                            break;
+                    }
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(Name)) Name = ListenPort.ToString();
+        }
+    }
+}
