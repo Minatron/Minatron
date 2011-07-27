3f9f395fe220080ecfbfc12e0ed6c1dd0f708634
Developer <minatron.git@gmail.com>
2011-07-27 22:28:11 +0400

    commit

--- CameraController/WebCommander.cs
+++ CameraController/WebCommander.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Web;
 using Newtonsoft.Json.Linq;
 
 namespace CameraController
