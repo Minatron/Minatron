62399f2221860d6c561d14f3182778ba34f26dbb
Developer <minatron.git@gmail.com>
2011-08-17 01:19:20 +0400

    add splashScreen add title of window add title of table change button title hide cameras errors

---
+++ ClientApplication.Infrastructure/Events/BackButtonPushEvent.cs
@@ -0,0 +1,10 @@
+﻿using Band.Storage.Minatron;
+using Microsoft.Practices.Composite.Presentation.Events;
+
+namespace Band.Client.Infrastructure.Events
+{
+    public class BackButtonPushEvent : CompositePresentationEvent<object>
+    {
+
+    }
+}
