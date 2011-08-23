c13722c60edefd27797ca481ecb03fd2fdbd27b2
Nefedov <solderer@gmail.com>
2011-08-23 10:14:11 +0400

    add event

---
+++ ClientApplication.Infrastructure/Events/BackButtonPushEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.Composite.Presentation.Events;
+
+namespace Band.Client.Infrastructure.Events
+{
+    public class BackButtonPushEvent : CompositePresentationEvent<object>
+    {
+    }
+}
